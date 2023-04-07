59dd77f1f824ccc0b1d12194c114a79a91bb43d9
ProgrammerOskar <oskarwrona123@gmail.com>
2023-04-07 16:26:18 +0200

    JS file with functions for 3 modes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let result
 const openNav = document.querySelector('.openNav')
 const closeNav = document.querySelector('.closeNav')
 const nav = document.querySelector('.navBar')
 const body = document.querySelector('body')
 const advanceCalcBtn = document.querySelector('.levelCalc')
 const simpleCalcBtn = document.querySelector('.simpleCalc')
+const proCalcBtn = document.querySelector('.proCalc')
 const board = document.querySelector('.down')
 // const buttons = querySelectorAll("")
+const upward = document.querySelector('.up')
 
 const openNavigation = () => {
 	nav.style.left = '10px'
@@ -21,9 +22,18 @@ const chooseAdvanced = () => {
 	//hover na .down
 	//rotateY (180 deg)
 	board.classList.add('active')
+	upward.classList.remove('activeResult')
+	board.classList.remove('activeProgramistic')
 }
 const chooseSimple = () => {
 	board.classList.remove('active')
+	board.classList.remove('activeProgramistic')
+	upward.classList.remove('activeResult')
+}
+
+const chooseProgramistic = () => {
+	board.classList.add('activeProgramistic')
+	upward.classList.add('activeResult')
 }
 
 document.onkeydown = function (evt) {
@@ -38,5 +48,4 @@ openNav.addEventListener('click', openNavigation)
 closeNav.addEventListener('click', closeNavigation)
 advanceCalcBtn.addEventListener('click', chooseAdvanced)
 simpleCalcBtn.addEventListener('click', chooseSimple)
-
-
+proCalcBtn.addEventListener('click', chooseProgramistic)
